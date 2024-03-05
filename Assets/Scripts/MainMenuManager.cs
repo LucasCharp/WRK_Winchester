868c357e2d024655ccecdcd078f361d79a51f1b4
MPCLaire <claire.poujois@gmail.com>
2024-03-05 09:23:47 +0100

    Finalisation zoom menu principal

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject objectToClick;
+    public GameObject objectDumpster;
+    public GameObject objectMenu;
     public Button buttonDumpster;
     public Button buttonMenu;
     public Camera mainCamera;
@@ -17,8 +18,13 @@ public class MainMenuManager : MonoBehaviour
     private float moveSpeed = 5f;
     private float moveSpeedBack = 8f;
     private float rotationSpeed = 5f;
+    public Button playButton;
+    private GameObject objectTouched;
+
     public List<AudioClip> son;
     public AudioSource soundPlayer;
+   
+    
 
     //public Transform initialCameraPosition;
     private bool isCameraClose = false;
@@ -41,27 +47,27 @@ public class MainMenuManager : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.gameObject == objectToClick)
+                if (hit.collider.gameObject == objectDumpster)
                 {
-                    if (objectToClick.CompareTag("Dumpster"))
-                        {
-                            StartCoroutine(MoveAndRotateCamera(targetDumpster.position, targetDumpster.rotation));
-                        }
-                        else if (objectToClick.CompareTag("Menu"))
-                    {
-                        StartCoroutine(MoveAndRotateCamera(targetMenu.position, targetMenu.rotation));
-                    }
+                    objectTouched = hit.collider.gameObject;
+                    StartCoroutine(MoveAndRotateCamera(targetDumpster.position, targetDumpster.rotation));
                 }
-                
+                else if (hit.collider.gameObject == objectMenu)
+                {
+                    objectTouched = hit.collider.gameObject;
+                    StartCoroutine(MoveAndRotateCamera(targetMenu.position, targetMenu.rotation));
+                    Debug.Log("Je vais vers le menu");
+                }
             }
         }
     }
 
     IEnumerator MoveAndRotateCamera(Vector3 targetPosition, Quaternion targetRotation)
     {
-        if (objectToClick.CompareTag("Dumpster"))
+        if (objectTouched.CompareTag("Dumpster"))
         {
-            Debug.Log("Je touche");
+            playButton.gameObject.SetActive(false);
+            Debug.Log("Je touche la poubelle");
             soundPlayer.clip = son[1];
             ClickSound();
             // Tant que la distance entre la caméra et la position cible est supérieure à une petite marge
@@ -75,13 +81,14 @@ public class MainMenuManager : MonoBehaviour
 
                 yield return null; // Attend une frame
 
-                objectToClick.GetComponent<Collider>().enabled = false;
+                objectDumpster.GetComponent<Collider>().enabled = false;
             }
             buttonDumpster.gameObject.SetActive(true);// met le bouton en visible après que la caméra soit arrivée, pour pas spam dessus
         }
-        if (objectToClick.CompareTag("Menu"))
+        if (objectTouched.CompareTag("Menu"))
         {
-            Debug.Log("Je touche");
+            playButton.gameObject.SetActive(false);
+            Debug.Log("Je touche le menu");
             ClickSound();
             // Tant que la distance entre la caméra et la position cible est supérieure à une petite marge
             while (Vector3.Distance(mainCamera.transform.position, targetPosition) > 0.1f)
@@ -94,18 +101,18 @@ public class MainMenuManager : MonoBehaviour
 
                 yield return null; // Attend une frame
 
-                objectToClick.GetComponent<Collider>().enabled = false;
+                objectMenu.GetComponent<Collider>().enabled = false;
             }
             buttonMenu.gameObject.SetActive(true);// met le bouton en visible après que la caméra soit arrivée, pour pas spam dessus
         }
 
     }
 
-    public void OnBackButtonClicked()
+    public void OnButtonDumpster()
     {
-        StartCoroutine(MoveAndRotateCameraBack(initialPosition.position, initialPosition.rotation));
+        StartCoroutine(MoveCameraBackDump(initialPosition.position, initialPosition.rotation));
     }
-    IEnumerator MoveAndRotateCameraBack(Vector3 targetPosition, Quaternion targetRotation)
+    IEnumerator MoveCameraBackDump(Vector3 targetPosition, Quaternion targetRotation)
     {
         soundPlayer.clip = son[2];
         buttonDumpster.gameObject.SetActive(false);
@@ -124,7 +131,35 @@ public class MainMenuManager : MonoBehaviour
         // Une fois que la caméra est revenue à la position cible, désactive le bouton retour, réactive le clic sur l'objet
         
         isCameraClose = false;
-        objectToClick.GetComponent<Collider>().enabled = true;
+        objectDumpster.GetComponent<Collider>().enabled = true;
+        playButton.gameObject.SetActive(true);
+    }
+
+    public void OnButtonMenu()
+    {
+        StartCoroutine(MoveCameraBackMenu(initialPosition.position, initialPosition.rotation));
+    }
+    IEnumerator MoveCameraBackMenu(Vector3 targetPosition, Quaternion targetRotation)
+    {
+        soundPlayer.clip = son[2];
+        buttonMenu.gameObject.SetActive(false);
+        // Tant que la caméra n'est pas revenu à la position cible
+        while (mainCamera.transform.position != targetPosition)
+        {
+            // Déplace la caméra progressivement vers la position cible
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, targetPosition, moveSpeedBack * Time.deltaTime);
+
+            // Oriente la caméra progressivement vers la rotation cible
+            mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            yield return null; // Attend une frame
+        }
+
+        // Une fois que la caméra est revenue à la position cible, désactive le bouton retour, réactive le clic sur l'objet
+
+        isCameraClose = false;
+        objectMenu.GetComponent<Collider>().enabled = true;
+        playButton.gameObject.SetActive(true);
     }
 
     public void OnPlayClicked()
