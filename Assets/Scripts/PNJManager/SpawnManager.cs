9f5cf86948ac1fe3c2c4eb7a9674a65b0e64c9b0
LucasCharp <lucascharp22@gmail.com>
2024-03-09 19:44:31 +0100

    Spawn des ennemis
    Les ennemis spawent sur le trottoir à un intervalle de temps modifiable grâce à une variable.

---
+++ Assets/Scripts/PNJManager/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] pnjPrefabs; // Tableau contenant vos préfabs avec le tag "PNJ"
+    public float spawnInterval = 4f; // Intervalle entre chaque spawn en secondes
+
+    void Start()
+    {
+        // Appeler la fonction SpawnRandomPNJ au démarrage
+        InvokeRepeating("SpawnRandomPNJ", 0f, spawnInterval);
+    }
+
+    void SpawnRandomPNJ()
+    {
+        // Sélectionner un préfab de manière aléatoire
+        GameObject selectedPrefab = pnjPrefabs[Random.Range(0, pnjPrefabs.Length)];
+
+        // Utiliser la position du GameObject qui détient le script comme position de spawn
+        Vector3 spawnPosition = transform.position;
+
+        // Instancier le préfab à la position du GameObject
+        Instantiate(selectedPrefab, spawnPosition, Quaternion.identity);
+    }
+}
