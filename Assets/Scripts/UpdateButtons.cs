2ef49d9789d50f479929b805b38c04405a1dde71
MPCLaire <claire.poujois@gmail.com>
2024-03-09 15:15:54 +0100

    Amélioration sol

--- Assets/Scripts/UpdateButtons.cs
+++ Assets/Scripts/UpdateButtons.cs
@@ -9,13 +9,27 @@ public class UpgradeButtons : MonoBehaviour // NE PAS OUBLIER DE DISABLE LES BOU
     public Button buttonRefill;
     public Button buttonBar;
     public Button buttonRetourBar;
+
+    public Button buttonFloor;
+    public Button buttonFloorRed;
+    public Button buttonFloorBrown;
+    public Button buttonFloorWhite;
+    public Button buttonFloorRetour;
+
+    public Color redColor;
+    public Color brownColor;
+    public Color whiteColor;
+    public GameObject floor;
+
     public Transform targetBar;
     public Camera mainCamera;
     public MainSceneManager mainSceneManager;
+    public CameraRotation cameraRotation;
 
     private Vector3 initialCameraPosition;
     private Quaternion initialCameraRotation;
     private bool goToBar;
+    private bool floorCliqued;
 
     private float moveSpeed = 5f;
     private float rotationSpeed = 5f;
@@ -28,17 +42,25 @@ public class UpgradeButtons : MonoBehaviour // NE PAS OUBLIER DE DISABLE LES BOU
     {
         buttonRefill.gameObject.SetActive(false);
         buttonRetourBar.gameObject.SetActive(false);
+
+        buttonFloorRed.gameObject.SetActive(false);
+        buttonFloorBrown.gameObject.SetActive(false);
+        buttonFloorWhite.gameObject.SetActive(false);
+        buttonFloorRetour.gameObject.SetActive(false);
     }
 
 
 
     public void OnBarCliqued()
     {
+        buttonFloor.gameObject.SetActive(false);
+
         initialCameraPosition = mainCamera.transform.position; // récupère les valeurs de la caméra avant de la faire bouger pour la remettre en place à la fin
         initialCameraRotation = mainCamera.transform.rotation;
 
         StartCoroutine(MoveAndRotateCamera(targetBar.position, targetBar.rotation));
         goToBar = true;
+        cameraRotation.canRotate = false;
     }
     IEnumerator MoveAndRotateCamera(Vector3 targetPosition, Quaternion targetRotation)
     {
@@ -53,6 +75,10 @@ public class UpgradeButtons : MonoBehaviour // NE PAS OUBLIER DE DISABLE LES BOU
 
             yield return null; // Attend une frame
         }
+        if (Vector3.Equals(targetPosition, initialCameraPosition))
+        {
+            cameraRotation.canRotate = true; // rétablit le fait de pouvoir touner la caméra
+        }
         if (goToBar == true)
         {
             if (buttonBar.gameObject.activeSelf)
@@ -64,6 +90,7 @@ public class UpgradeButtons : MonoBehaviour // NE PAS OUBLIER DE DISABLE LES BOU
             else if (!buttonBar.gameObject.activeSelf)
             {
                 buttonBar.gameObject.SetActive(true);
+                buttonFloor.gameObject.SetActive(true);
                 goToBar = false;
             }
            
@@ -74,12 +101,24 @@ public class UpgradeButtons : MonoBehaviour // NE PAS OUBLIER DE DISABLE LES BOU
 
     public void OnRetourCLiqued()
     {
-        StartCoroutine(MoveAndRotateCamera(initialCameraPosition, initialCameraRotation));
+        if (floorCliqued == true)
+        {
+            cameraRotation.canRotate = true;
+            buttonFloor.gameObject.SetActive(true);
+            buttonFloorRed.gameObject.SetActive(false);
+            buttonFloorBrown.gameObject.SetActive(false);
+            buttonFloorWhite.gameObject.SetActive(false);
+            buttonFloorRetour.gameObject.SetActive(false);
+            buttonBar.gameObject.SetActive(true);
+            floorCliqued = false;
+        }
+        else StartCoroutine(MoveAndRotateCamera(initialCameraPosition, initialCameraRotation));
         if (goToBar == true)
         {
             buttonRefill.gameObject.SetActive(false);
             buttonRetourBar.gameObject.SetActive(false);
         }
+   
     }
 
     public void OnRefillCliqued()
@@ -89,8 +128,56 @@ public class UpgradeButtons : MonoBehaviour // NE PAS OUBLIER DE DISABLE LES BOU
 
     public void OnFloorCliqued()
     {
-        Debug.Log("Je clique");
+        floorCliqued = true;
+        cameraRotation.canRotate = false;
+        buttonFloor.gameObject.SetActive(false);
+        buttonFloorRed.gameObject.SetActive(true);
+        buttonFloorBrown.gameObject.SetActive(true);
+        buttonFloorWhite.gameObject.SetActive(true);
+        buttonFloorRetour.gameObject.SetActive(true);
+        buttonBar.gameObject.SetActive(false);
     }
+
+    public void OnRedFloorCliqued()
+    {
+        Renderer renderer = floor.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // Changez la couleur de tous les matériaux du Renderer
+            foreach (Material material in renderer.materials)
+            {
+                material.color = redColor;
+            }
+        }
+    }
+
+    public void OnBrownFloorCliqued()
+    {
+        Renderer renderer = floor.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // Changez la couleur de tous les matériaux du Renderer
+            foreach (Material material in renderer.materials)
+            {
+                material.color = brownColor;
+            }
+        }
+    }
+
+    public void OnWhiteFloorCliqued()
+    {
+        Renderer renderer = floor.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // Changez la couleur de tous les matériaux du Renderer
+            foreach (Material material in renderer.materials)
+            {
+                material.color = whiteColor;
+            }
+        }
+    }
+
+
 
     public void OnTableCliqued()
     {
