b6bd776be7616e341fb5171c46d32cbb528a2abe
LinoB <77000248+LinoBellanca@users.noreply.github.com>
2024-03-10 23:25:14 +0100

    Sound Sliders

--- Assets/Scripts/Audio Mixer/AudioMixerManager.cs
+++ Assets/Scripts/Audio Mixer/AudioMixerManager.cs
@@ -6,5 +6,14 @@ using UnityEngine.Audio;
 public class AudioMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+
+    public void SetSoundFXVolume(float level)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(level) * 20f);
+    }
     
+    public void SetMusicVolume(float level)
+    {
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(level) * 20f);
+    }
 }
