338786fad534cbbccf7994e0732621a1a3b9e89d
LucasCharp <lucascharp22@gmail.com>
2024-03-14 11:24:59 +0100

    Ma queue marche
    Enfin pour l'instant ils sont cinq a faire la queue maintenant il faut que je fasse que les portes s'ouvre quand la première personne de la file a l'autorisation d'entrer et que les autres suivent la file mais je suis positif.

--- Assets/Scripts/PNJManager/QueueManager.cs
+++ Assets/Scripts/PNJManager/QueueManager.cs
@@ -5,30 +5,33 @@ using UnityEngine.AI;
 
 public class QueueManager : MonoBehaviour
 {
-    Animator animator;
     public float numberOfPeopleInQueue;
     public bool isFull = false;
+    public int maxQueueSize = 5; // Ajustez cette valeur selon votre besoin
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PNJ"))
         {
             numberOfPeopleInQueue += 1;
+
+            // Vérifiez si la file d'attente est pleine
+            if (numberOfPeopleInQueue >= maxQueueSize)
+            {
+                isFull = true;
+                Debug.Log("File d'attente pleine !");
+            }
         }
     }
+
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("PNJ"))
         {
-            Debug.Log("il quitte la queue");
+            Debug.Log("Il quitte la queue");
             numberOfPeopleInQueue -= 1;
+
+            // Réinitialisez la condition de file d'attente pleine
             isFull = false;
         }
     }
