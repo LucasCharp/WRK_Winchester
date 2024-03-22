d9f1af3cced3b27394e1303b919f0104fd1fbe76
LinoB <77000248+LinoBellanca@users.noreply.github.com>
2024-03-22 01:16:18 +0100

    Tutori Partie 3
    Son + Merge sur la map Lucas2

--- Assets/Scripts/TutorialMenuController.cs
+++ Assets/Scripts/TutorialMenuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TutorialMenuController : MonoBehaviour
@@ -7,9 +8,12 @@ public class TutorialMenuController : MonoBehaviour
     public GameObject tutorialMenu;
     public GameObject tutoImage;
 
+    public AudioClip son;
+
     // Méthode appelée lorsque le bouton est cliqué
     public void OpenTutorialMenu(GameObject whichImage)
     {
+        SFXManager.instance.PlaySoundFXClip(son, transform, 1f);
         tutoImage = whichImage;
         // Activer le menu du tutoriel
         tutoImage.SetActive(true);
@@ -20,11 +24,28 @@ public class TutorialMenuController : MonoBehaviour
 
     public void QuitTutorialMenu(GameObject whichImage)
     {
+        SFXManager.instance.PlaySoundFXClip(son, transform, 1f);
         tutoImage = whichImage;
         // Désactiver le menu du tutoriel
         tutoImage.SetActive(false);
 
         // Enlever la pause
         Time.timeScale = 1f;
+    }
+
+    public void OpenTutorialSlide(GameObject whichImage)
+    {
+        SFXManager.instance.PlaySoundFXClip(son, transform, 1f);
+        tutoImage = whichImage;
+        // Activer le menu du tutoriel
+        tutoImage.SetActive(true);
+    }
+    public void QuitTutorialSlide(GameObject whichImage)
+    {
+        SFXManager.instance.PlaySoundFXClip(son, transform, 1f);
+        tutoImage = whichImage;
+        // Désactiver le menu du tutoriel
+        tutoImage.SetActive(false);
+
     }
 }
